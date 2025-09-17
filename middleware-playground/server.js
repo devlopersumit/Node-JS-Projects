62c8544daf7f3fcbb1a4b143980845d3e1035200
devlopersumit <sumitsj2002@gmail.com>
2025-09-17 17:37:03 +0530

    imported all the modules into server.js file

--- middleware-playground/server.js
+++ middleware-playground/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const logger = require("./middlewares/logger");
+const errorHandler = require("./middlewares/errorHandler");
+
+//Routes
+const publicRoutes = require("./routes/publicRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+const errorRoutes = require("./routes/errorRoutes");
+
 const app = express();
 
 app.use(express.json());
 
+app.use(logger);
+
+app.use(publicRoutes);
+app.use(errorRoutes);
+app.use(dashboardRoutes);
+
+app.use(errorHandler);
 
 app.listen(3000, () => console.log("Server running successfulyy on port 3000"));
