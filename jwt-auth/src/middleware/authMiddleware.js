e1c0944c92120802fd6604024f9f57c026312696
devlopersumit <sumitsj2002@gmail.com>
2025-09-19 19:27:52 +0530

    completed authMiddleware

---
+++ jwt-auth/src/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+// Middleware to verify token
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid token' });
+  }
+};
+
+export default verifyToken;
