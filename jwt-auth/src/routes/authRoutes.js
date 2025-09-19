0d158af9dc5776da2342648ae0e7b71aea833ec0
devlopersumit <sumitsj2002@gmail.com>
2025-09-19 19:26:06 +0530

    completed authRoutes

---
+++ jwt-auth/src/routes/authRoutes.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+const router = express.Router();
+
+// SIGNUP
+router.post('/signup', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ msg: 'User already exists' });
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // Create a new user
+        const newUser = new User({ email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ msg: 'User registered successfully' });
+    } catch (err) {
+        res.status(500).json({ msg: 'Server Error' });
+        console.error(err);
+    }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ msg: 'User not found' });
+        // Check if password matches
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+        // Create and sign JWT token
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({message: 'Login successful', token });
+    } catch (err) {
+        res.status(500).json({ msg: 'Server Error' });
+        console.error(err);
+    }
+});
+
+//VERIFY TOKEN PROTECTED ROUTES
+router.get('/verify', (req, res) => {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(401).json({ msg: 'Token is not valid' });
+        res.json({ msg: 'Token is valid', user });
+    });
+});
+
+export default router;
